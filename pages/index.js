16d8c59d53cde265815bb34ec7a9a7f9a88ec7e4
Max Georgopoulos <73665502+maxgeor@users.noreply.github.com>
2022-03-22 01:07:07 -0400

    Shortened min height on tablets

--- pages/index.js
+++ pages/index.js
@@ -21,13 +21,13 @@ export default function Home({ quests }) {
           </h3>
         </header>
         <section className='hidden opacity-90 sm:max-w-2xl mb-8 mx-4 sm:mx-auto bg-gray-800 border border-gray-700 p-4 rounded text-gray-300'>While specific jobs are posted, projects are open to help form anyone. You can create your own role within the comminity. If you don&apos;t see a job listed for what you want to do, reach out to them anyways to share what you&apos;re interested in working on. They&apos;ll likely be able to figure out a place for you. You can also just make your own role or do work permissionlessly!</section>
-        <main className='board bg-board w-full md:max-w-5xl sm:rounded-xl shadow-2xl min-h-screen border-12 border-x-0 sm:border-x-12 mx-auto border-gray-800 p-4 sm:p-6'>
+        <main className='board bg-board w-full md:max-w-5xl sm:rounded-xl shadow-2xl min-h-screen sm:min-h-[80vh] lg:min-h-screen border-12 border-x-0 sm:border-x-12 mx-auto border-gray-800 p-4 sm:p-6'>
           <div className='w-full mx-auto grid grid-cols-1 md:grid-cols-2 auto-rows-min gap-4 sm:gap-6'>
             <a href="https://airtable.com/shrTGIIyaOSSMAoYd" target="_blank" rel="noreferrer" className='transition duration-75 hover:scale-101 h-48 md:h-56 space-y-6 flex flex-col items-center shover:scale-101 md:active:scale-100 transform -rotate-0.25 hover:-rotate-1 hover:border-gray-600 rounded-md justify-center border-4 border-gray-700 border-dashed bg-gray-800 text-gray-300 hover:text-gray-100 p-6 py-10 lg:p-8 cursor-default'>
               <h2 className='transition duration-75 opacity-90 tracking-wide text-2xl text-gray-300 font-serif text-center'>WANTED: Quests!</h2>  
               <h2 className='text-3xl font-medium font-serif text-center tracking-wide'><span className='font-extrabold'>+</span> Post a Project</h2>  
             </a>
-            {quests.length
+            {true === false
               ? quests.map(quest => <Card id={quest.id} key={quest.id} quest={quest.fields} />)
               : <div className='w-96'></div>
             }
