7afaeb75fc7cae297112e93f25730453686877f5
Max Georgopoulos <73665502+maxgeor@users.noreply.github.com>
2022-03-29 22:35:31 -0400

    Spacing

--- pages/quest/[id].js
+++ pages/quest/[id].js
@@ -24,10 +24,10 @@ export default function Quest({ quest }) {
       <div className='w-full max-auto sm:pb-24'>
         <header className='flex justify-center my-10'>
           <Link href='/'>
-            <div className='shadow-inner board-logo bg-board flex flex-col items-center cursor-default pt-4 pb-5 px-6 transition duration-75 hover:scale-101 md:active:scale-100 transform -rotate-1 hover:-rotate-2 border-4 border-board-logo-border hover:border-gray-700 rounded-md hover:bg-gray-800 w-min'>
+            <a className='shadow-inner board-logo bg-board flex flex-col items-center cursor-default pt-4 pb-5 px-6 transition duration-75 hover:scale-101 md:active:scale-100 transform -rotate-1 hover:-rotate-2 border-4 border-board-logo-border hover:border-gray-700 rounded-md hover:bg-gray-800 w-min'>
               <span className='whitespace-nowrap text-sm uppercase tracking-wider leading-4'>The</span>
               <h1 className='whitespace-nowrap text-2xl text-center uppercase tracking-wider font-medium'>Quest Board</h1>
-            </div>
+            </a>
           </Link>
         </header>
         <main className={`py-8 pb-40 px-6 sm:px-16 mx-auto min-h-[110vh] h-full flex flex-col items-center bg-gray-200 text-gray-900 sm:max-w-3xl rounded-xl rounded-b-none sm:rounded-b-xl shadow-2xl`}>
@@ -68,7 +68,7 @@ export default function Quest({ quest }) {
               Interested? Message <span className='text-base font-medium py-0.5 px-1.5 bg-gray-300/90 border border-gray-400/15 rounded font-mono text-gray-900 mx-0.5'>{quest.fields.poster.trim()}</span> in the <Link href={'https://discord.com/invite/KuYyKXam9G'} ><a target="_blank" rel="noreferrer" className='underline decoration-1 decoration-dashed hover:decoration-solid decoration-gray-500 hover:decoration-gray-900 underline-offset-4 text-gray-900'>Loot Discord</a></Link>.
             </p>
           </div>
-          <article id="quest-description" className='max-w-prose mr-auto first:mt-0 mt-14 text-xl text-gray-700'>
+          <article id="quest-description" className='max-w-prose mr-auto first:mt-0 mt-12 text-xl text-gray-700'>
             <ReactMarkdown>{quest.fields.project_description}</ReactMarkdown>
           </article>
         </main>
