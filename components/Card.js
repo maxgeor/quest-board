328d16e03b61ded802eb0507609635a81065437e
Max Georgopoulos <73665502+maxgeor@users.noreply.github.com>
2022-04-01 00:11:23 -0400

    Made layout work with longer reward names

--- components/Card.js
+++ components/Card.js
@@ -8,39 +8,40 @@ export default function Card({ id, quest }) {
 
   return (
     <Link href={`quest/${id}`}>
-      <a className='md:min-h-[13rem] h-full max-w-md w-full md:max-w-none mx-auto shadow-lg hover:shadow-xl active:shadow-lg z-10 min-h-full space-y-9 flex flex-col md:justify-between items-center hover:scale-101 md:active:scale-100 transition duration-75 rounded-md transform md:odd:-rotate-0.25 odd:hover:-rotate-1 md:even:rotate-0.25 even:hover:rotate-1 bg-gray-200 hover:bg-gray-100 text-black p-6 md:p-8 cursor-default'>
-        <div className='max-w-sm mx-auto space-y-4'>
+      {/* <a className='md:min-h-[13rem] h-full max-w-md w-full md:max-w-none mx-auto shadow-lg hover:shadow-xl active:shadow-lg z-10 min-h-full flex flex-col md:justify-between items-center hover:scale-101 md:active:scale-100 transition duration-75 rounded-md transform md:odd:-rotate-0.25 odd:hover:-rotate-1 md:even:rotate-0.25 even:hover:rotate-1 bg-gray-100 hover:bg-gray-100 text-black cursor-default'> */}
+      <a className='flex flex-col justify-between items-center space-y-5 p-7 h-min min-h-[13rem] md:min-h-[14rem] max-w-md w-full md:max-w-none mx-auto shadow-lg hover:shadow-xl active:shadow-lg z-10 md:justify-between hover:scale-101 md:active:scale-100 transition duration-75 rounded-md transform md:odd:-rotate-0.25 odd:hover:-rotate-1 md:even:rotate-0.25 even:hover:rotate-1 bg-gray-200 hover:bg-gray-100 text-black cursor-default'>
+        <div className='max-w-sm mx-auto space-y-3'>
           <h3 className='text-2xl font-medium text-gray-900 font-serif text-center mt-0'>
             {bundledRoles} needed!
           </h3>
-          <p className='text-xl font-serif text-gray-800 text-center mx-6'>
-            <span className='text-gray-600 text-sm md:text-base mr-0.5'>for</span> {quest.project}
+          <p className='text-lg font-serif text-gray-700 text-center mx-7'>
+            <span className='text-gray-500 text-sm mr-0.5'>for</span> {quest.project}
           </p>  
         </div>
-        <div className='w-full flex items-center justify-center flex-wrap gap-y-1 space-x-4 mx-8'>
-          <span className='text-sm font-mono text-center text-gray-600'>{quest.posted_on}</span>
-          {quest.has_reward && <>
-          <span className='text-gray-400'>/</span>
-          <div className='flex items-center font-serif space-x-1.5'>
-            <span className='transform rotate-3'>
-              <Bag /> 
-            </span>
-            <span className='text-gray-800'>
-              {quest.custom_reward
-                ? quest.custom_reward
-                : <span className='space-x-1'>
-                    <span className='text-xl'>{quest.reward_amount}</span>
-                    <span className='text-sm'>
-                      {quest.custom_token
-                        ? quest.custom_token.toUpperCase().trim()
-                        : quest.reward_type}  
-                    </span>
+        {quest.has_reward && <>
+        {/* <div className='w-full p-3 py-2 rounded bg-gray-300/40 border border-gray-300 flex justify-center font-serif space-x-1.5'> */}
+
+        <div className='rounded bg-gray-300/50 p-3 w-full flex justify-center font-serif space-x-1.5'>
+          <span className='transform rotate-3'>
+            <Bag /> 
+          </span>
+          <span className='text-gray-800'>
+            {quest.custom_reward
+              ? quest.custom_reward
+              : <span className='space-x-1'>
+                  <span className='text-lg'>{quest.reward_amount}</span>
+                  <span className='text-sm'>
+                    {quest.custom_token
+                      ? quest.custom_token.toUpperCase().trim()
+                      : quest.reward_type}  
                   </span>
-              }
-            </span>
-          </div>  
-          </>}
+                </span>
+            }
+          </span>
         </div>
+        </>}
+
+        <span className='text-sm font-mono text-center text-gray-500'>{quest.posted_on}</span>
       </a>
     </Link>
   )
--- pages/quest/[id].js
+++ pages/quest/[id].js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import Hand from '../../icons/Hand'
 import Bag from '../../icons/Bag'
 import Group from '../../icons/Group'
 import ReactMarkdown from 'react-markdown'
@@ -32,29 +33,31 @@ export default function Quest({ quest }) {
         </header>
         <main className={`py-8 pb-40 px-6 sm:px-16 mx-auto min-h-[110vh] h-full flex flex-col items-center bg-gray-200 text-gray-900 sm:max-w-3xl rounded-xl rounded-b-none sm:rounded-b-xl shadow-2xl`}>
           <span className='text-sm font-mono text-center text-gray-600 mb-6'>{quest.fields.posted_on}</span>
-          <div className='w-full relative h-full space-y-8'>
+          <div className='w-full relative h-full space-y-12'>
             <h1 className='font-semibold text-center text-4xl sm:text-5xl mx-4 text-gray-800'>{bundledRoles} Needed!</h1>
-            <div className='text-xl flex flex-wrap items-center justify-center gap-y-1 space-x-4'>
-              <div className='flex items-center font-serif space-x-2.5'>
-                <Group /> 
+            <div className='space-y-3 bg-gray-300/40 p-5 rounded-lg text-lg flex flex-col max-w-lg mx-auto'>
+              <div className=' flex items-center font-serif space-x-3'>
+                <span className='flex items-center justify-center flex-shrink-0 h-7 w-7 self-start'>
+                  <Group /> 
+                </span>
                 {quest.fields.project_link 
                   ? <Link href={`${quest.fields.project_link}`}>
-                      <a target="_blank" rel="noreferrer" className='text-gray-800 text-xl underline decoration-1 decoration-dashed hover:decoration-solid decoration-gray-500 hover:decoration-gray-900 underline-offset-4'>{quest.fields.project.trim()}</a>
+                      <a target="_blank" rel="noreferrer" className='text-lg underline decoration-1 decoration-dashed hover:decoration-solid underline-offset-4'>{quest.fields.project.trim()}</a>
                     </Link>
-                  : <span className='text-gray-800 text-xl'>{quest.fields.project.trim()}</span>
+                  : <span className='text-gray-800 text-lg'>{quest.fields.project.trim()}</span>
                 }
-              </div> 
+              </div>
               {quest.fields.has_reward && <>
-                <span className='text-gray-400'>/</span>
-                <div className='flex items-center font-serif space-x-1.5'>
-                  <span className='transform rotate-3'>
+                <hr className='border-gray-300 ml-10' />
+                <div className='flex items-center font-serif space-x-3'>
+                  <span className='flex items-center justify-center flex-shrink-0 h-7 w-7 self-start'>
                     <Bag />
                   </span>
                   <span className='text-gray-800 space-x-1'>
                     {quest.fields.custom_reward
                       ? quest.fields.custom_reward
                       : <span>
-                          <span className='text-xl'>{quest.fields.reward_amount}</span>
+                          <span className='text-lg'>{quest.fields.reward_amount}</span>
                           <span className='text-sm'>
                             {quest.fields.custom_token
                               ? quest.fields.custom_token.toUpperCase().trim()
@@ -65,10 +68,16 @@ export default function Quest({ quest }) {
                   </span>
                 </div>  
               </>}
+              <hr className='border-gray-300 ml-10' />
+              <div className=' flex items-center font-serif space-x-3'>
+                <span className='flex items-center justify-center flex-shrink-0 h-7 w-7 self-start'>
+                  <Hand /> 
+                </span>
+                <p className=' text-lg'>
+                  Interested? Message <span className='text-base font-medium py-0.5 px-1.5 bg-gray-300 border border-gray-400/30 rounded font-mono text-gray-900 mx-0.5'>{quest.fields.poster.trim()}</span> in the <Link href={'https://discord.com/invite/KuYyKXam9G'} ><a target="_blank" rel="noreferrer" className='underline decoration-1 decoration-dashed hover:decoration-solid underline-offset-4 text-gray-900'>Loot Discord</a></Link>.
+                </p>
+              </div>
             </div>
-            <p className='w-full text-lg text-center p-4 px-6  border-y border-gray-300 text-gray-600'>
-              Interested? Message <span className='text-base font-medium py-0.5 px-1.5 bg-gray-300/90 border border-gray-400/15 rounded font-mono text-gray-900 mx-0.5'>{quest.fields.poster.trim()}</span> in the <Link href={'https://discord.com/invite/KuYyKXam9G'} ><a target="_blank" rel="noreferrer" className='underline decoration-1 decoration-dashed hover:decoration-solid decoration-gray-500 hover:decoration-gray-900 underline-offset-4 text-gray-900'>Loot Discord</a></Link>.
-            </p>
           </div>
           <article id="quest-description" className='max-w-prose mr-auto first:mt-0 mt-12 text-xl text-gray-700'>
             <ReactMarkdown>{quest.fields.project_description}</ReactMarkdown>
